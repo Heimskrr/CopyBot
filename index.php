e2dc12ff593c878992cbd6d01cc5084d635361e4
Heimskrr <110385445+Heimskrr@users.noreply.github.com>
2022-08-01 21:41:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,16 +4,16 @@
 /////////////////////////// CONFIGURATION /////////////////////////////
 
 // add your Twitter Developer credits
-$APIkey="KWYXJVyFFAu8qFI12xkZJrF13";
-$APIsecretKey="nHXZ0o3S8rRVXNjF3TLUt0Cph5B6EN5ssDKMpxHmlZWy0kEyoh";
-$AccesToken="1525924924812148736-72hHrtWFs8PiP58asZkpDcI6IN7J8W";
-$AccesTokenSecret="6b2t1mcHYjktDPH8KxSwtV3YWAwsks9M5WawyIaTkoIPb";
+$APIkey="";
+$APIsecretKey="";
+$AccesToken="";
+$AccesTokenSecret="";
 
 // add Twitter name (@) of the account you want to copy tweets
-$victim="@mGbaum1204";
+$victim="";
 
 // add your Twitter name (@)
-$me="@burry_bot";
+$me="";
 
 ///////////////////////////////////////////////////////////////////
 
